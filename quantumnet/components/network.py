54e523fbfc571c550e47af0001bce072887d0975
Arthur Henrique <111461334+artuenric@users.noreply.github.com>
2024-05-13 18:02:51 -0300

    Update network.py
    Add draw.
    Método add_host reformulado com padrões de desenvolvimento.
    Adicionado Logger.

--- quantumnet/components/network.py
+++ quantumnet/components/network.py
@@ -1,25 +1,56 @@
 import networkx as nx
+from ..objects import Logger
+from ..components import Host
 
 class Network():
     """
     Um objeto para utilizar como rede.
     """
     def __init__(self) -> None:
-        self.G = None
+        # Sobre a rede
+        self._graph = nx.Graph()
         self._channels = None
         self._topology = None
+        self._hosts = {}
         self._controller = None
+        # Sobre a execução
+        self.logger = Logger.get_instance()
+        
+    @property
+    def hosts(self):
+        """
+        Dicionário com os hosts da rede. No padrão {host_id: host}.
+
+        Returns:
+            dict : Dicionário com os hosts da rede.
+        """
+        return self._hosts
     
-    def add_host(self, host):
+    def draw(self):
         """
-        Adiciona um nó à rede.
+        Desenha a rede.
         """
+        nx.draw(self._graph, with_labels=True)
+    
+    def add_host(self, host: Host):
+        """
+        Adiciona um host à rede no dicionário de hosts, e o host_id ao grafo da rede.
+            
+            Args:
+                host (Host): O host a ser adicionado.
+        """
+        # Adiciona o host ao dicionário de hosts, se não existir
+        if host.host_id not in self._hosts:
+            self._hosts[host.host_id] = host
+            Logger.get_instance().debug(f'Host {host.host_id} adicionado aos hosts da rede.')
+        
         # Adiciona o nó ao grafo da rede, se não existir
-        if not self.G.has_node(host):
-            self.G.add_node(host)
+        if not self._graph.has_node(host.host_id):
+            self._graph.add_node(host.host_id)
+            Logger.get_instance().debug(f'Nó {host.host_id} adicionado ao grafo da rede.')
         
         # Adiciona as conexões do nó ao grafo da rede, se não existirem
         for connection in host.connections:
-            if not self.G.has_edge(host, connection):
-                self.G.add_edge(host, connection)
-        
+            if not self._graph.has_edge(host.host_id, connection):
+                self._graph.add_edge(host.host_id, connection)
+                Logger.get_instance().debug(f'Conexões do {host.host_id} adicionados ao grafo da rede.')
