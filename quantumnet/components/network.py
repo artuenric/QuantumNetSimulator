d6684f074c1b3cd85cdba0f1404b341f91ab0cde
Arthur Henrique <111461334+artuenric@users.noreply.github.com>
2024-05-14 18:33:53 -0300

    Topologias prontas e novos métodos
    Add novos métodos.
    Host_id agora é do tipo Int.

--- quantumnet/components/network.py
+++ quantumnet/components/network.py
@@ -26,6 +26,36 @@ class Network():
         """
         return self._hosts
     
+    @property
+    def graph(self):
+        """
+        Grafo da rede.
+
+        Returns:
+            nx.Graph : Grafo da rede.
+        """
+        return self._graph
+    
+    @property
+    def nodes(self):
+        """
+        Nós do grafo da rede.
+
+        Returns:
+            list : Lista de nós do grafo.
+        """
+        return self._graph.nodes()
+    
+    @property
+    def edges(self):
+        """
+        Arestas do grafo da rede.
+
+        Returns:
+            list : Lista de arestas do grafo.
+        """
+        return self._graph.edges()
+    
     def draw(self):
         """
         Desenha a rede.
@@ -36,21 +66,69 @@ class Network():
         """
         Adiciona um host à rede no dicionário de hosts, e o host_id ao grafo da rede.
             
-            Args:
-                host (Host): O host a ser adicionado.
+        Args:
+            host (Host): O host a ser adicionado.
         """
         # Adiciona o host ao dicionário de hosts, se não existir
-        if host.host_id not in self._hosts:
+        if host.host_id not in self._hosts:        
             self._hosts[host.host_id] = host
             Logger.get_instance().debug(f'Host {host.host_id} adicionado aos hosts da rede.')
-        
+        else:
+            raise Exception(f'Host {host.host_id} já existe nos hosts da rede.')
+            
         # Adiciona o nó ao grafo da rede, se não existir
         if not self._graph.has_node(host.host_id):
             self._graph.add_node(host.host_id)
             Logger.get_instance().debug(f'Nó {host.host_id} adicionado ao grafo da rede.')
-        
+            
         # Adiciona as conexões do nó ao grafo da rede, se não existirem
         for connection in host.connections:
             if not self._graph.has_edge(host.host_id, connection):
                 self._graph.add_edge(host.host_id, connection)
                 Logger.get_instance().debug(f'Conexões do {host.host_id} adicionados ao grafo da rede.')
+    
+    def get_host(self, host_id: int) -> Host:
+        """
+        Retorna um host da rede.
+
+        Args:
+            host_id (int): ID do host a ser retornado.
+
+        Returns:
+            Host : O host com o host_id fornecido.
+        """
+        return self._hosts[host_id]
+       
+    def set_ready_topology(self, topology_name: str, *args: int) -> str:
+        """
+        Cria um grafo com uma das topologias prontas para serem utilizadas. 
+        São elas: Grade, Linha, Anel. Os nós são numerados de 0 a n-1, onde n é o número de nós.
+
+        Args: 
+            topology_name (str): Nome da topologia a ser utilizada.
+            **args (int): Argumentos para a topologia. Geralmente, o número de hosts.
+        
+        """
+        # Nomeia a topologia da rede
+        self._topology = topology_name
+    
+        # Cria o grafo da topologia escolhida
+        if topology_name == 'Grade':
+            if len(args) != 2:
+                raise Exception('Para a topologia Grade, são necessários dois argumentos.')
+            self._graph = nx.grid_2d_graph(*args)
+        elif topology_name == 'Linha':
+            if len(args) != 1:
+                raise Exception('Para a topologia Linha, é necessário um argumento.')
+            self._graph = nx.path_graph(*args)
+        elif topology_name == 'Anel':
+            if len(args) != 1:
+                raise Exception('Para a topologia Anel, é necessário um argumento.')
+            self._graph = nx.cycle_graph(*args)
+
+        # Converte os labels dos nós para inteiros
+        self._graph = nx.convert_node_labels_to_integers(self._graph)
+
+        # Cria os hosts e adiciona ao dicionário de hosts
+        for node in self._graph.nodes():
+            self._hosts[node] = Host(node)
