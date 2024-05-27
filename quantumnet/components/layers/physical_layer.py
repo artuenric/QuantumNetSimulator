a1b57c2cfa18ac9257e28ca07abae186f2b7d5e2
David Tavares <david.tavares@icen.ufpa.br>
2024-05-27 14:55:11 -0300

    Criação do echp_on_demand e echp_on_replay
    foi adicionado uma função de medir apenas um qubit e os dois protocolos

--- quantumnet/components/layers/physical_layer.py
+++ quantumnet/components/layers/physical_layer.py
@@ -63,6 +63,21 @@ class PhysicalLayer():
         # Adicionar esse EPR em algum lugar
         return epr
     
+    
+    def fidelity_measurement_only_one(self, qubit: Qubit):
+        """
+        Mede a fidelidade de um qubit.
+
+        Args:
+            qubit (Qubit): Qubit.
+
+        Returns:
+            float: Fidelidade do qubit.
+        """
+        fidelity = qubit.fidelity()
+        self.logger.log(f'A fidelidade do qubit {qubit} é {fidelity}')
+        return fidelity
+    
     def fidelity_measurement(self, qubit1: Qubit, qubit2: Qubit):
         """
         Mede a fidelidade entre dois qubits.
@@ -78,7 +93,7 @@ class PhysicalLayer():
         self.logger.log(f'A fidelidade entre o qubit {qubit1} e o qubit {qubit2} é {fidelity}')
         return fidelity
     
-    def Entanglement_Creation_Heralding_Protocol(self, alice: Host, bob: Host):
+    def entanglement_creation_heralding_protocol(self, alice: Host, bob: Host):
         """ 
         Protocolo de criação de emaranhamento com sinalização.
         
@@ -89,7 +104,7 @@ class PhysicalLayer():
         qubit1 = alice.get_last_qubit()
         qubit2 = bob.get_last_qubit()
         self.create_epr_pair(qubit1, qubit2)
-    
+
         # Checa a fidelidade
         fidelity = self.fidelity_measurement(qubit1, qubit2)
         
@@ -102,3 +117,23 @@ class PhysicalLayer():
             return True
         self.logger.log('O protocolo de criação de emaranhamento falhou.')
         return False
+    
+    #    1.1. ECHP_on_demand:
+
+    #-Fidelidade_inicial: Fidelidade_qubit1* Fidelidade_qubit2
+    def echp_on_demand(self, alice: Host, bob: Host):
+    #-Proabilidade de Sucesso do ECHP: Prob_replay_epr_create * Fidelidade_qubit1* Fidelidade_qubit2
+        prob_replay_epr_create = 0.9
+        qubit1 = alice.get_last_qubit()
+        qubit2 = bob.get_last_qubit()
+        fidelidade_qubit1 = self.fidelity_measurement_only_one(qubit1)
+        fidelidade_qubit2 = self.fidelity_measurement_only_one(qubit2)
+        proabilidade_de_sucesso_do_echp = prob_replay_epr_create * fidelidade_qubit1* fidelidade_qubit2
+        self.logger.log(f'A probabilidade de sucesso do ECHP é {proabilidade_de_sucesso_do_echp}')
+        return proabilidade_de_sucesso_do_echp
+
+    def echp_on_replay(self, alice: Host, bob: Host):
+        #o ECHP_on_replay é quando a fidelidade decaiu e o protocolo é refeito
+        
+    
+    
